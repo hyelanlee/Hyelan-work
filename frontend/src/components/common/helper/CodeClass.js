9cf64ab0d7881417bfaf9630ca99ecceef3e3368
Hyelan <hyelanlove@gmail.com>
2023-05-06 20:24:26 +0900

    44

--- frontend/src/components/common/helper/CodeClass.js
+++
@@ -1,6 +0,0 @@
-/*
- * CodeClass
- */
-
-export const Default = { iTag: 'Q', iInId: 'P250', iInCode1: '001', iInCode2: '', iInCode3: '', iInCode4: '', iInCode5: '', iInCode6: '', iFieldnm: null, iSort: null };
-export const Cust = { iTag: 'Q', iAccunit: '001', iCustType1: '', iCustOutcd: '', iCustNm: '', iTruncNm: '', iTaxNo: '', iOwner: '' };
